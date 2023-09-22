acccb04246bfd1d207593d57a5880953391312b8
Msofyan14 <mxsofyan14@gmail.com>
2023-09-22 22:55:35 +0700

    search and filtering function

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,22 +1,30 @@
 import Filter from "@/components/Filter";
+import Header from "@/components/Header";
 import ResourceCard from "@/components/ResourceCard";
 import SearchForm from "@/components/SearchForm";
-import { getResources } from "@/sanity/action";
-import React from "react";
+import { getResources, getResourcesPlaylist } from "@/sanity/action";
 
-const page = async () => {
+export const revalidate = 900;
+
+interface Props {
+  searchParams: { [key: string]: string | undefined };
+}
+
+const page = async ({ searchParams }: Props) => {
   const resources = await getResources({
-    query: "",
-    category: "",
+    query: searchParams?.query || "",
+    category: searchParams?.category || "",
     page: "1",
   });
 
-  console.log(resources);
+  const resourcesPlaylist = await getResourcesPlaylist();
+
+  console.log(resourcesPlaylist);
 
   return (
     <main className="flex-center paddings mx-auto w-full max-w-screen-2xl flex-col">
       <section className="nav-padding w-full">
-        <div className="flex-center realative min-h-[274px] w-full flex-col rounded-xl bg-banner bg-cover bg-center text-center text-white">
+        <div className="flex-center relative min-h-[274px] w-full flex-col rounded-xl bg-banner bg-cover bg-center text-center text-white">
           <h1 className="sm:heading1 heading2 mb-6 text-center text-white">
             Javascript Mastery Resources
           </h1>
@@ -27,16 +35,49 @@ const page = async () => {
 
       <Filter />
 
-      <section className="flex-center mt-6 w-full flex-col sm:mt-20">
-        Header
-        <div className="mt-12 flex w-full flex-wrap justify-center gap-16 sm:justify-start">
-          {resources?.length > 0 ? (
-            resources.map((resource: any) => <ResourceCard />)
-          ) : (
-            <p className="body-regular text-white-400">No resource found</p>
-          )}
-        </div>
-      </section>
+      {searchParams?.query || searchParams?.category ? (
+        <section className="flex-center mt-6 w-full flex-col sm:mt-20">
+          <Header
+            query={searchParams?.query || ""}
+            category={searchParams?.category || ""}
+          />
+          <div className="mt-12 flex w-full flex-wrap justify-center gap-16 sm:justify-start">
+            {resources?.length > 0 ? (
+              resources.map((resource: any) => (
+                <ResourceCard
+                  key={resource._id}
+                  title={resource.title}
+                  image={resource.image}
+                  downloadNumber={resource.views}
+                  downloadLink={resource.downloadLink}
+                />
+              ))
+            ) : (
+              <p className="body-regular text-white-400">No resource found</p>
+            )}
+          </div>
+        </section>
+      ) : (
+        resourcesPlaylist.map((item: any) => (
+          <section
+            key={item._id}
+            className="flex-center mt-6 w-full flex-col sm:mt-20"
+          >
+            <h1 className="heading3 self-start text-white-800">{item.title}</h1>
+            <div className="mt-12 flex w-full  flex-wrap justify-center gap-16 sm:justify-start">
+              {item.resources.map((resource: any) => (
+                <ResourceCard
+                  key={resource._id}
+                  title={resource.title}
+                  image={resource.image}
+                  downloadNumber={resource.views}
+                  downloadLink={resource.downloadLink}
+                />
+              ))}
+            </div>
+          </section>
+        ))
+      )}
     </main>
   );
 };
